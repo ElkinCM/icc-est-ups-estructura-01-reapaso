12e7ac79038f08791712f38aa64d4c7fa9105529
EMCMateo <mateo5678moligmail.com>
2025-04-22 08:28:44 -0500

    Commit Arreglado

--- src/App.java
+++ src/App.java
@@ -1,14 +1,46 @@
-
+import Controllers.PersonaController;
 import Models.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Persona[] personas = new Persona[] {
-
+            new Persona("Juan", 25),
+            new Persona("Maria", 30),
+            new Persona("Carlos", 22),
+            new Persona("Ana", 28),
+            new Persona("Luis", 35),
+            new Persona("Sofia", 27),
+            new Persona("Miguel", 40),
+            new Persona("Laura", 32),
+            new Persona("Pedro", 29),
+            new Persona("Elena", 26),
         };
 
-        /// TODO: GENERAR INSTNACIA DE CONTROLLE Y ORDENAR EL METODO DESPUES BUSCAR POR
-        /// LA EDAD
+        PersonaController controller = new PersonaController();
+        controller.ordenarPorEdad(personas);
 
+        System.out.println("Personas ordenadas por edad:");
+        for (Persona persona : personas) {
+            System.out.println(persona.getNombre() + " : " + persona.getEdad());
+        }
+        System.out.println("--------------------------------------------------");
+        int edadBuscada = 40;
+        Persona encontrada = controller.buscarPorEdad(personas, edadBuscada);
+        System.out.println("Buscando persona con edad " + edadBuscada + "...");
+        if (encontrada != null) {
+            System.out.println("Persona encontrada: " + encontrada.getNombre() + " - " + encontrada.getEdad());
+        } else {
+            System.out.println("No se encontró una persona con la edad " + edadBuscada);
+        }
+        System.out.println("--------------------------------------------------");
+        //buscar persona con edad 99
+        edadBuscada = 99;
+        encontrada = controller.buscarPorEdad(personas, edadBuscada);
+        System.out.println("Buscando persona con edad " + edadBuscada + "...");
+        if (encontrada != null) {
+            System.out.println("Persona encontrada: " + encontrada.getNombre() + " - " + encontrada.getEdad());
+        } else {
+            System.out.println("No se encontró una persona con la edad " + edadBuscada);
+        }
     }
 }
--- src/Controllers/PersonaController.java
+++ src/Controllers/PersonaController.java
@@ -7,14 +7,41 @@ import Models.Persona;
  */
 public class PersonaController {
     /**
+     * Imprimir el arreglo de Persona en consola.
+     * Ordena el arreglo por edad
+     * imprime el arreglo ordenado
+     *
      * Método para ordenar un arreglo de Persona por edad utilizando el algoritmo de
      * inserscion .
-     * 
+     *
+     * @param personas Array de Persona a ordenar.
+     */
+    public void imprimirArreglo(Persona[] personas) {
+        for (Persona persona : personas) {
+            System.out.println(persona.toString());
+        }
+    }
+    /**
+     * Método para ordenar un arreglo de Persona por edad utilizando el algoritmo de
+     * inserción.
+     *
      * @param personas Array de Persona a ordenar.
      */
     public void ordenarPorEdad(Persona[] personas) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        for (int i = 1; i < personas.length; i++) {
+            Persona persona = personas[i];
+            int j = i - 1;
+            while (j >= 0 && personas[j].getEdad() > persona.getEdad()) {
+                personas[j + 1] = personas[j];
+                j = j - 1;
+
+            }
+            personas[j + 1] = persona;
+        }
+        
     }
+    
+
 
     /**
      * Método para buscar la primera persona con una edad específica en un array de
@@ -25,8 +52,15 @@ public class PersonaController {
      * @return La primera Persona con la edad especificada, o null si no se
      *         encuentra.
      */
+
     public Persona buscarPorEdad(Persona[] personas, int edad) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        for (Persona persona : personas) {
+            if (persona.getEdad() == edad) {
+                return persona;
+            }
+        }
+        return null;
 
     }
+
 }
